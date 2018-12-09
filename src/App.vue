b6608d15c4394246225a412dc0ef21c19fbec0ea
BH-NOTHING <2388322143@qq.com>
2018-12-09 17:21:24 +0800

    add orderRecord

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,45 @@
 <template>
   <div id="app">
     <!--<chanePhone/>-->
-    <collection/>
+    <!--<collection/>-->
+    <orderRecord/>
   </div>
 </template>
 
 <script>
 import chanePhone from './components/chanePhone'
 import collection from './components/collection'
+import orderRecord from './components/orderRecord'
 
 export default {
   name: 'App',
   components: {
     chanePhone,
-    collection
+    collection,
+    orderRecord
   }
+  // B5B5B5
 }
 </script>
 
 <style>
 #app {
   font-family: 'Arial', Helvetica, Arial, sans-serif;
+}
+#app input::-webkit-input-placeholder {
+  color: #B5B5B5;
+}
+#app input:-moz-placeholder {
+  color: #B5B5B5;
+}
+#app input::-moz-placeholder {
+  color: #B5B5B5;
+}
+#app input::-ms-input-placeholder {
+  color: #B5B5B5;
+}
+ul{
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/components/orderRecord.vue
@@ -0,0 +1,111 @@
+<template>
+  <div>
+    <div class="order-title">安装订单</div>
+    <div class="order-record">
+      <ul>
+        <li class="record-details" v-for="item in orders">
+          <div class="record-info">
+            <div>
+              <span class="info-good">{{item.good}}</span>
+              <span class="info-price">{{item.price}}</span>
+            </div>
+            <div class="info-bottom">
+              <span class="info-date">{{item.date}}</span>
+              <span class="info-status">{{item.status}}</span>
+            </div>
+          </div>
+          <div class="record-next">x</div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'orderRecord',
+  data () {
+    return {
+      orders: [
+        {
+          good: '坐垫(5座)',
+          price: '30元',
+          date: '2018-11-29 15:45:02',
+          status: '订单完成'
+        },
+        {
+          good: '坐垫(5座)',
+          price: '30元',
+          date: '2018-11-29 15:45:02',
+          status: '订单完成'
+        },
+        {
+          good: '坐垫(5座)',
+          price: '30元',
+          date: '2018-11-29 15:45:02',
+          status: '订单完成'
+        },
+        {
+          good: '坐垫(5座)',
+          price: '30元',
+          date: '2018-11-29 15:45:02',
+          status: '订单完成'
+        }
+      ]
+    }
+  },
+  methods:{
+    next(){
+      console.log('下一步')
+    },
+    collectionRecord(){
+      console.log('收款记录')
+    }
+  }
+}
+</script>
+<style scoped>
+.order-title{
+  text-align: center;
+  color: #2679E9;
+  padding: 10px;
+  border-bottom: 1px solid #E5E5E5;
+}
+.order-record li{
+  list-style-type: none;
+}
+.order-record  .record-details{
+  display: flex;
+  border-bottom: 2px solid #E5E5E5;
+}
+.order-record  .record-details .record-info{
+  width: 100%;
+  height: 50px;
+  padding: 16px;
+}
+.order-record  .record-details  .record-next{
+    width: 10px;
+    line-height: 82px;
+}
+.order-record  .record-details .record-info .info-bottom{
+  padding-top: 6px;
+}
+.order-record  .record-details .record-info .info-good{
+  font-size: 16px;
+  font-weight: bold;
+}
+.order-record  .record-details .record-info .info-price{
+  float: right;
+  font-size: 16px;
+  font-weight: bold;
+}
+.order-record  .record-details .record-info .info-date{
+  font-size: 12px;
+  color: #A1A1A1;
+}
+.order-record  .record-details .record-info .info-status{
+  float: right;
+  font-size:12px;
+  color: #2679E9;
+}
+</style>

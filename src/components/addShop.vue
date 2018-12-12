2f9e7306a78e2e466b067081764807266de1be61
BH-NOTHING <2388322143@qq.com>
2018-12-12 22:19:51 +0800

    update

--- src/components/addShop.vue
+++ src/components/addShop.vue
@@ -41,11 +41,14 @@
         <span class="info-name">详细地址</span>
         <input placeholder="请输入详细地址" type="text" />
       </div>
-      <div class="info-item">
+      <div class="info-item info-map-mark">
         <svg class="icon-required" style="width: 0.5em; height: 0.5em;vertical-align: middle;fill: red;overflow: hidden;" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2025"><path d="M876.8 659.2 620.8 512l256-147.2c25.6-12.8 38.4-51.2 19.2-76.8-12.8-25.6-51.2-38.4-76.8-19.2l-256 147.2L563.2 121.6C569.6 89.6 544 64 512 64S454.4 89.6 454.4 121.6l0 294.4-256-147.2C179.2 249.6 140.8 262.4 128 288c-12.8 25.6-6.4 64 19.2 76.8l256 147.2-256 147.2c-25.6 12.8-38.4 51.2-19.2 76.8 12.8 19.2 32 25.6 51.2 25.6 6.4 0 19.2 0 25.6-6.4l256-147.2 0 294.4C454.4 934.4 480 960 512 960s57.6-25.6 57.6-57.6L569.6 608l256 147.2c6.4 6.4 19.2 6.4 25.6 6.4 19.2 0 38.4-12.8 51.2-25.6C908.8 710.4 902.4 672 876.8 659.2z" p-id="2026"></path></svg>
         <span class="info-name">地址标注</span>
         <input placeholder="请输入验证码" type="text" />
       </div>
+    </div>
+    <div>
+      我已经阅读并同意<a href="#">协议条款</a>
     </div>
   </div>
 </template>
@@ -58,7 +61,7 @@ export default {
     }
   },
   methods: {
-    addPhoto(){
+    addPhoto () {
       console.log('添加图片')
     }
   }
@@ -79,7 +82,8 @@ export default {
   background: red;
   margin: 10px 0;
 }
-.inputInfo .info-item.info-phone-id{
+.inputInfo .info-item.info-phone-id,
+.inputInfo .info-item.info-map-mark{
   padding: 10px 15px;
   border-bottom: 4px solid #ccc;
 }

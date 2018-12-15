5a9436f84fe7b462dcc5c6979a6d180e771fe470
BH-NOTHING <2388322143@qq.com>
2018-12-15 23:48:30 +0800

    update

--- src/components/addShop.vue
+++ src/components/addShop.vue
@@ -50,6 +50,7 @@
      <div class="filed-div">
        <svg class="icon-required" style="width: 0.5em; height: 0.5em;vertical-align: middle;fill: red;overflow: hidden;" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2025"><path d="M876.8 659.2 620.8 512l256-147.2c25.6-12.8 38.4-51.2 19.2-76.8-12.8-25.6-51.2-38.4-76.8-19.2l-256 147.2L563.2 121.6C569.6 89.6 544 64 512 64S454.4 89.6 454.4 121.6l0 294.4-256-147.2C179.2 249.6 140.8 262.4 128 288c-12.8 25.6-6.4 64 19.2 76.8l256 147.2-256 147.2c-25.6 12.8-38.4 51.2-19.2 76.8 12.8 19.2 32 25.6 51.2 25.6 6.4 0 19.2 0 25.6-6.4l256-147.2 0 294.4C454.4 934.4 480 960 512 960s57.6-25.6 57.6-57.6L569.6 608l256 147.2c6.4 6.4 19.2 6.4 25.6 6.4 19.2 0 38.4-12.8 51.2-25.6C908.8 710.4 902.4 672 876.8 659.2z" p-id="2026"></path></svg>
        <div class="add-mark-btn" @click="addMark">地图标注</div>
+       <div class="add-mark-tip-btn" @click="addMark">(非常重要)</div>
        <mt-field label="地图标注" readonly=true></mt-field>
      </div>
      <div v-if="pickerShow" v-click-outside="hidePicker" class="picker-wrapper">
@@ -198,11 +199,12 @@ export default {
 }
 .filed-div{
   position: relative;
+  padding: 0 10px;
 }
 .icon-required{
   position: absolute;
   top: calc(50% - 2px);
-  left: 0;
+  left: 10;
   z-index:99;
 }
 .filed-div .mint-cell-wrapper{
@@ -221,9 +223,17 @@ export default {
 }
 .filed-div .add-mark-btn{
   position: absolute;
-  top: calc(50% - 12px);
-  left: calc(50% - 10px);
+  top: calc(50% - 10px);
+  left: calc(50% - 56px);
   z-index: 10;
+  color: #2A72C5;
+}
+.filed-div .add-mark-tip-btn{
+  position: absolute;
+  top: calc(50% - 10px);
+  right: 30px;
+  z-index: 10;
+  color: #ff0000;
 }
 .shop-photo{
   padding: 16px;
@@ -271,6 +281,8 @@ export default {
 .shop-qq-map{
   width: 100%;
   height: 100%;
+  display: flex;
+  flex-direction: column;
   text-align: center;
 }
 .preserve-pos-btn{

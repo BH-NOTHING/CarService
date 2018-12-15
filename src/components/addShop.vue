9b29fb56be6e212c8f11369988704a8c674a58c5
BH-NOTHING <2388322143@qq.com>
2018-12-15 18:29:46 +0800

    add cityData

--- src/components/addShop.vue
+++ src/components/addShop.vue
@@ -51,7 +51,7 @@
       <!--我已经阅读并同意<a href="#">协议条款</a>-->
     <!--</div>-->
   <!--</div>-->
- <div>
+ <div class="add-shop">
    <div class="filed-div">
      <svg class="icon-required" style="width: 0.5em; height: 0.5em;vertical-align: middle;fill: red;overflow: hidden;" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2025"><path d="M876.8 659.2 620.8 512l256-147.2c25.6-12.8 38.4-51.2 19.2-76.8-12.8-25.6-51.2-38.4-76.8-19.2l-256 147.2L563.2 121.6C569.6 89.6 544 64 512 64S454.4 89.6 454.4 121.6l0 294.4-256-147.2C179.2 249.6 140.8 262.4 128 288c-12.8 25.6-6.4 64 19.2 76.8l256 147.2-256 147.2c-25.6 12.8-38.4 51.2-19.2 76.8 12.8 19.2 32 25.6 51.2 25.6 6.4 0 19.2 0 25.6-6.4l256-147.2 0 294.4C454.4 934.4 480 960 512 960s57.6-25.6 57.6-57.6L569.6 608l256 147.2c6.4 6.4 19.2 6.4 25.6 6.4 19.2 0 38.4-12.8 51.2-25.6C908.8 710.4 902.4 672 876.8 659.2z" p-id="2026"></path></svg>
      <mt-field label="店铺名称" placeholder="请输入店铺名称" v-model="shopName" rows=5></mt-field>
@@ -83,22 +83,31 @@
        <div v-if="photoList.length <= 5" class="add-photo-btn" @click="addPhoto"></div>
      </div>
    </div>
-   <div class="filed-div">
+   <div @click="showPicker" class="filed-div">
      <svg class="icon-required" style="width: 0.5em; height: 0.5em;vertical-align: middle;fill: red;overflow: hidden;" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2025"><path d="M876.8 659.2 620.8 512l256-147.2c25.6-12.8 38.4-51.2 19.2-76.8-12.8-25.6-51.2-38.4-76.8-19.2l-256 147.2L563.2 121.6C569.6 89.6 544 64 512 64S454.4 89.6 454.4 121.6l0 294.4-256-147.2C179.2 249.6 140.8 262.4 128 288c-12.8 25.6-6.4 64 19.2 76.8l256 147.2-256 147.2c-25.6 12.8-38.4 51.2-19.2 76.8 12.8 19.2 32 25.6 51.2 25.6 6.4 0 19.2 0 25.6-6.4l256-147.2 0 294.4C454.4 934.4 480 960 512 960s57.6-25.6 57.6-57.6L569.6 608l256 147.2c6.4 6.4 19.2 6.4 25.6 6.4 19.2 0 38.4-12.8 51.2-25.6C908.8 710.4 902.4 672 876.8 659.2z" p-id="2026"></path></svg>
-     <mt-field label="详细地址" placeholder="请输入详细地址" v-model="detailAd"></mt-field>
+     <mt-field label="详细地址" placeholder="请输入详细地址" :value="getAd"></mt-field>
    </div>
-   <!--<mt-field label="birthday" placeholder="Input birthday" type="date" v-model="birthday"></mt-field>-->
-   <!--<mt-field label="introduction" placeholder="introduction" type="textarea" rows="4" v-model="introduction"></mt-field>-->
+   <div v-if="pickerShow" v-click-outside="hidePicker" class="picker-wrapper">
+     <mt-picker :slots="myAddressSlots" @change="onMyAddressChange"></mt-picker>
+   </div>
  </div>
 </template>
 
 <script>
 import { Field } from 'mint-ui'
 import { chooseFile } from '../lib/xhrUploadFile'
+import cityData from '../lib/cityData'
+import { Picker } from 'mint-ui';
+import ClickOutside from 'vue-click-outside'
+console.log(cityData)
 export default {
   name: 'addShop',
   components: {
-    'mt-field': Field
+    'mt-field': Field,
+    'mt-picker': Picker
+  },
+  directives: {
+    ClickOutside
   },
   props: {
     // shopName: {
@@ -128,13 +137,62 @@ export default {
       detailAd: '',
       showInfoId: true,
       count: 60,
-      photoList: []
+      photoList: [],
+      myAddressSlots: [
+        {
+          flex: 1,
+          defaultIndex: 1,
+          values: Object.keys(cityData),  //省份数组
+          className: 'slot1',
+          textAlign: 'center'
+        }, {
+          divider: true,
+          content: '-',
+          className: 'slot2'
+        }, {
+          flex: 1,
+          values: [],
+          className: 'slot3',
+          textAlign: 'center'
+        },
+        {
+          divider: true,
+          content: '-',
+          className: 'slot4'
+        },
+        {
+          flex: 1,
+          values: [],
+          className: 'slot5',
+          textAlign: 'center'
+        }
+      ],
+      myAddressProvince:'',
+      myAddressCity:'',
+      myAddresscounty:'',
+      pickerShow: false
+    }
+  },
+  mounted(){
+    this.$nextTick(() => { //vue里面全部加载好了再执行的函数  （类似于setTimeout）
+      this.myAddressSlots[0].defaultIndex = 0
+      // 这里的值需要和 data里面 defaultIndex 的值不一样才能够初始化
+      //因为我没有看过源码（我猜测是因为数据没有改变，不会触发更新）
+    });
+  },
+  computed: {
+    getAd(){
+      let ad = ''
+      ad = this.myAddressProvince ? this.myAddressProvince + ad : ad
+      ad = this.myAddressCity ? this.myAddressCity + ad : ad
+      ad =this. myAddresscounty ? this.myAddresscounty + ad : ad
+      return ad
     }
   },
   methods: {
     addPhoto () {
       console.log('添加图片')
-      uploadImg()
+      chooseFile('image')
     },
     getInfoId(){
       this.showInfoId = false
@@ -146,11 +204,32 @@ export default {
           this.showInfoId = true
         }
       },1000)
+    },
+    onValuesChange(picker, values) {
+      console.log(values)
+      if (values[0] > values[1]) {
+        picker.setSlotValue(1, values[0]);
+      }
+    },
+    onMyAddressChange(picker, values) {
+      if(cityData[values[0]]){  //这个判断类似于v-if的效果（可以不加，但是vue会报错，很不爽）
+        picker.setSlotValues(1,Object.keys(cityData[values[0]])); // Object.keys()会返回一个数组，当前省的数组
+        picker.setSlotValues(2,cityData[values[0]][values[1]]); // 区/县数据就是一个数组
+        this.myAddressProvince = values[0];
+        this.myAddressCity = values[1];
+        this.myAddresscounty = values[2];
+      }
+    },
+    hidePicker(){
+      this.pickerShow = false
+    },
+    showPicker(){
+      this.pickerShow = true
     }
   }
 }
-</script scoped>
-<style>
+</script>
+<style scoped>
 .filed-div{
   position: relative;
 }
@@ -206,6 +285,15 @@ export default {
 .shop-photo .photo-wrapper .newPhoto img{
   width: 100%;
   height: 100%;
+}
+.add-shop .picker-wrapper{
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+}
+.add-shop .picker-wrapper .picker{
+  overflow: hidden;
+  background: #EDEDED;
 }
 /*.inputInfo .icon-required{*/
   /*bottom: 0.08em;*/
